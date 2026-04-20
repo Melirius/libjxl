ee08e8831a410db007e67c1ce68b2c05b77794cf
Melirius <melirius@gmail.com>
2026-04-20 21:06:41 +0200

    Efforts update (suggested by Galaxy4594)

--- lib/jxl/transcode_jpeg/enc_jpeg_search.h
+++ lib/jxl/transcode_jpeg/enc_jpeg_search.h
@@ -111,30 +111,18 @@ struct JPEGCtxEffortParams {
                 /*overhead_aware_tail=*/true,
                 /*refine_iters=*/1,
                 /*refine_radius=*/4,
-                /*use_bicluster_search=*/true,
+                /*use_bicluster_search=*/false,
                 /*bicluster_outer_iters=*/2,
                 /*bicluster_proto_budget_per_pass=*/128,
                 /*bicluster_row_budget=*/kMaxClusters,
                 /*optimize_passes_num=*/-1,
                 /*bicluster_refine_thresholds=*/false};
       case SpeedTier::kGlacier:
-        return {/*ac_hist_model=*/JPEGTranscodeACModel::kRawAI,
-                /*keep_top_k=*/0,
-                /*rank_m_target=*/0,
-                /*rank_iters=*/0,
-                /*main_m_target=*/256,
-                /*main_iters=*/8,
-                /*overhead_aware_tail=*/true,
-                /*refine_iters=*/2,
-                /*refine_radius=*/8,
-                /*use_bicluster_search=*/true,
-                /*bicluster_outer_iters=*/2,
-                /*bicluster_proto_budget_per_pass=*/128,
-                /*bicluster_row_budget=*/kMaxClusters,
-                /*optimize_passes_num=*/-1,
-                /*bicluster_refine_thresholds=*/false};
       case SpeedTier::kTectonicPlate:
       default:
+        // Non-modular effort 11 is clamped to `kGlacier` in `EncodeFrame()`, so
+        // JPEG lossless recompression's highest reachable search lives here.
+        // Full search space is 58 factorizations.
         return {/*ac_hist_model=*/JPEGTranscodeACModel::kRawAI,
                 /*keep_top_k=*/0,
                 /*rank_m_target=*/0,
@@ -144,7 +132,7 @@ struct JPEGCtxEffortParams {
                 /*overhead_aware_tail=*/true,
                 /*refine_iters=*/5,
                 /*refine_radius=*/16,
-                /*use_bicluster_search=*/true,
+                /*use_bicluster_search=*/false,
                 /*bicluster_outer_iters=*/2,
                 /*bicluster_proto_budget_per_pass=*/128,
                 /*bicluster_row_budget=*/kMaxClusters,
